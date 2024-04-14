8f2956b2e2f9b312652ed5b90a9205cad5f5fb35
JohannesG <johannes.gevlja@tktk.ee>
2024-04-14 23:55:30 +0300

    add week7 code

---
+++ Functions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Channels;
+
+Console.WriteLine("Are you coming or leaving? (in/out)");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+
+
+PrintHello();
+PrintGoodBye();
+static void PrintHello()
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
+
